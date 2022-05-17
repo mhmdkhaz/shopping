166238a59d20ca1ec6cb7deeebc7db20abacf14c
mhmdkhaz <mhmdkh0957@gmail.com>
2022-05-17 19:49:03 +0300

    fix header and js

--- js/text.js
+++ js/text.js
@@ -15,55 +15,53 @@ activeElement.forEach((element) => {
 let btnToTop = document.getElementById("toTop");
 let containrToTop = document.getElementById("backToTop");
 
-window.onscroll =  function () {
+window.onscroll = function () {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     containrToTop.style.bottom = "5%";
-  }else{
+  } else {
     containrToTop.style.bottom = "100%";
   }
-  btnToTop.onclick = ()=>{
+  btnToTop.onclick = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-  }
-}
+  };
+};
 // end  in back to top
-  // start in filter element
-  let btnChose = document.querySelectorAll(".buttons-choose button");
-  let imgChose = document.querySelectorAll(".filterImg");
+// start in filter element
+let btnChose = document.querySelectorAll(".buttons-choose button");
+let imgChose = document.querySelectorAll(".filterImg");
 
-  btnChose.forEach((btn)=>{
-    btn.addEventListener("click" , chooseElement);
-  })
-  function chooseElement() {
-    imgChose.forEach((contentimg)=>{
-      contentimg.style.display = "none";
-    })
-    document.querySelectorAll(this.dataset.cat).forEach((el)=>{
-      el.style.display = "block";
-    })
-  }
+btnChose.forEach((btn) => {
+  btn.addEventListener("click", chooseElement);
+});
+function chooseElement() {
+  imgChose.forEach((contentimg) => {
+    contentimg.style.display = "none";
+  });
+  document.querySelectorAll(this.dataset.cat).forEach((el) => {
+    el.style.display = "block";
+  });
+}
 
+// click image and show img in box
+function SetSrcInImageAndGet() {
+  let FullScreen = document.querySelectorAll(".cakeImg img");
+  let ShowImage = document.getElementById("showImgFullScreen");
+  let setSrcImg = document.getElementById("imgFullScreen");
+  let hide = document.getElementById("close");
 
-  // click image and show img in box
-  function SetSrcInImageAndGet() {
-    let FullScreen = document.querySelectorAll(".cakeImg img");
-    let ShowImage = document.getElementById("showImgFullScreen");
-    let setSrcImg = document.getElementById("imgFullScreen");
-    let hide = document.getElementById("close");
-
-    FullScreen.forEach((BoxImg) => {
-      BoxImg.addEventListener("click", () => {
-        let getSrc = BoxImg.getAttribute("src");
-        ShowImage.style.visibility = "visible";
-        setSrcImg.src = `${getSrc}`;
-      });
+  FullScreen.forEach((BoxImg) => {
+    BoxImg.addEventListener("click", () => {
+      let getSrc = BoxImg.getAttribute("src");
+      ShowImage.style.visibility = "visible";
+      setSrcImg.src = `${getSrc}`;
     });
-    hide.addEventListener("click", () => {
-      ShowImage.style.visibility = "hidden";
-    });
-  }
-  SetSrcInImageAndGet();
-
+  });
+  hide.addEventListener("click", () => {
+    ShowImage.style.visibility = "hidden";
+  });
+}
+SetSrcInImageAndGet();
 
 // start in cntact change imgaes 2 second
 let contactImg = document.querySelector(".imgcontact img");
@@ -71,18 +69,18 @@ let i = 0;
 
 function changeImgaesContact() {
   let ArrImgContact = [
-    "../images/undraw_personal_email_re_4lx7.svg",
-    "../images/undraw_profile_data_re_v81r.svg",
-    "../images/undraw_profile_details_re_ch9r.svg"
+    "images/undraw_personal_email_re_4lx7.svg",
+    "images/undraw_profile_data_re_v81r.svg",
+    "images/undraw_profile_details_re_ch9r.svg",
   ];
   setInterval(() => {
-    contactImg.style.opacity = "1";
     contactImg.src = `${ArrImgContact[i]}`;
     i++;
-    // contactImg.style.opacity = "0";
+    contactImg.style.top = "0%";
     if (i > ArrImgContact.length) {
-      contactImg.src = `${ArrImgContact[i -= i]}`;   
+      contactImg.src = `${ArrImgContact[(i -= i)]}`;
+      contactImg.style.top = "100%";
     }
-  },1000);
+  }, 1000);
 }
 changeImgaesContact();
